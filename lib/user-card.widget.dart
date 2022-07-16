f0ff7bed49bd15b9e76351635fc91e9697c22ef6
stobertonf <stobertonf@gmail.com>
2022-07-16 18:45:21 -0300

    feat(User-Card): Finishing User-Card.

--- lib/user-card.widget.dart
+++ lib/user-card.widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo/components/avatar.widget.dart';
 
 class UserCard extends StatelessWidget {
   const UserCard({Key? key}) : super(key: key);
@@ -15,6 +16,40 @@ class UserCard extends StatelessWidget {
           alignment: Alignment.bottomCenter,
           image: ExactAssetImage("assets/images/notification.png"),
         ),
+      ),
+      width: double.infinity,
+      alignment: Alignment.center,
+      child: Column(
+        children: <Widget>[
+          TDAvatar(
+            width: 80,
+            path: "https://placehold.it/80",
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Text(
+            "Stoberton Francisco",
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          Container(
+            height: 20,
+            child: FlatButton(
+              child: const Text(
+                "Sair",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {},
+            ),
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+        ],
       ),
     );
   }
