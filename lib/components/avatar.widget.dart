e7d61cbb0519c89c5cbb3e0b6e850eebee81b896
stobertonf <stobertonf@gmail.com>
2022-07-16 16:41:51 -0300

    feat(Avatar): Creating The TBAvatar File.

---
+++ lib/components/avatar.widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TDAvatar extends StatelessWidget {
+  const Avatar({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
