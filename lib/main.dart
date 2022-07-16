09aa602c5dfee85cc70a726297f4ce6a1fbe7c5d
stobertonf <stobertonf@gmail.com>
2022-07-16 18:04:39 -0300

    feat(Login): Changing HomePage.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:todo/components/avatar.widget.dart';
 import 'package:todo/themes/app.theme.dart';
-import 'package:todo/components/button.widget.dart';
+import 'package:todo/views/login.view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,38 +14,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Todos',
       theme: appTheme(),
-      home: const HomePage(),
+      home: LoginView(),
       debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: <Widget>[
-          const SizedBox(
-            height: 40,
-          ),
-          TDButton(
-            width: 300,
-            text: "Text",
-            callback: () {},
-            image: "assets/images/google.png",
-          ),
-          const SizedBox(
-            height: 40,
-          ),
-          TDAvatar(
-            width: 120,
-            path: "https//placehold.it/200",
-          ),
-        ],
-      ),
     );
   }
 }
