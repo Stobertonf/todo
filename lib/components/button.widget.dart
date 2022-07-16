e78b5d189352ae3bb9744ff48e9e4d26cdd7b1d3
stobertonf <stobertonf@gmail.com>
2022-07-16 16:31:23 -0300

    feat(TBButton): Creating The FlaButton.

--- lib/components/button.widget.dart
+++ lib/components/button.widget.dart
@@ -1,10 +1,49 @@
 import 'package:flutter/material.dart';
 
 class TDButton extends StatelessWidget {
-  const TDButton({Key? key}) : super(key: key);
+  final String text;
+  final String image;
+  final Function callback;
+  final double width;
+
+  TDButton({
+    @required this.text,
+    @required this.callback,
+    this.image,
+    this.width,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      width: width != 0 ? width : 250,
+      color: Theme.of(context).primaryColor,
+      child: FlaButton(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            image != null
+                ? Image.asset(
+                    image,
+                    width: 32,
+                  )
+                : const SizedBox(),
+            image != null
+                ? const SizedBox(
+                    width: 10,
+                  )
+                : const SizedBox(),
+            Text(
+              text,
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+        onPressed: () {},
+      ),
+    );
   }
 }
