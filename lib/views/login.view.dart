65a3a391c2a98c97c8d7c5cd9f5944dd05a77ab8
stobertonf <stobertonf@gmail.com>
2022-07-16 18:11:30 -0300

    feat(Login): Finishing Login-Page.

--- lib/views/login.view.dart
+++ lib/views/login.view.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
 
 class LoginView extends StatelessWidget {
-  const LoginView({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(30),
+          child: Card(
+            child: Column(
+              children: <Widget>[
+                const SizedBox(
+                  width: double.infinity,
+                ),
+                Image.asset(
+                  "assets/images/notification.png",
+                  width: 250,
+                ),
+                const Text(
+                  "Olá desconhecido",
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TDButton(
+                    text: "Login com Google",
+                    image: "assets/images/google.png",
+                    callback: () {}),
+                const SizedBox(
+                  height: 40,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
