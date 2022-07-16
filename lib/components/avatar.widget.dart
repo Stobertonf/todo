93666d267e80d8e445e91652127cac69270557c3
stobertonf <stobertonf@gmail.com>
2022-07-16 16:57:13 -0300

    feat(Avatar): Finishing The TBAvatar File.

--- lib/components/avatar.widget.dart
+++ lib/components/avatar.widget.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
 
 class TDAvatar extends StatelessWidget {
-  const Avatar({ Key? key }) : super(key: key);
+  final String path;
+  final double width;
+
+  TDAvatar({@required  this.path, @required  this.width})
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      width: width,
+       height: width,
+       padding: const EdgeInsets.all(20),
+       decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(width),
+          image: DecorationImage(
+            image: NetworkImage(path),
+          ),
+       ),
+       color: Colors.black.withOpacity(0.2),
     );
   }
 }
