8123df2f61e60ad3e2e3f4b8287aac8b9cdd83da
stobertonf <stobertonf@gmail.com>
2022-07-17 00:08:24 -0300

    feat(Nav-Bar): Creating NavBar file.

---
+++ lib/widgets/navbar.widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NavBar extends StatelessWidget {
+  const NavBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
