46c748d5f7f6efb1bbebcda3428b05306963ef42
stobertonf <stobertonf@gmail.com>
2022-07-17 15:40:02 -0300

    feat(Login-Google): Creating Login Controller File.

---
+++ lib/controllers/login.controller.dart
@@ -0,0 +1,10 @@
+import 'package:todo/user.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class LoginController {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+  Future login() async {}
+}
