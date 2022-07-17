b4c2690257aa78fd329d23fa00dbbd138391b40b
stobertonf <stobertonf@gmail.com>
2022-07-17 11:54:24 -0300

    feat(Create-Todo): Finishing Create-Todo.

--- lib/views/create-todo.view.dart
+++ lib/views/create-todo.view.dart
@@ -1,10 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:todo/components/button.widget.dart';
+import 'package:todo/widgets/user-card.widget.dart';
 
 class CreateTodoView extends StatelessWidget {
   const CreateTodoView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            const UserCard(),
+            Padding(
+              padding: const EdgeInsets.all(40.0),
+              child: Form(
+                child: Column(
+                  children: <Widget>[
+                    TextFormField(
+                      keyboardType: TextInputType.text,
+                      decoration: InputDecoration(
+                        labelText: "Tarefa",
+                        labelStyle: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      ),
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Theme.of(context).primaryColor,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        top: 20,
+                      ),
+                      child: Text(
+                        "17/07/2022",
+                        style: TextStyle(
+                          fontSize: 34,
+                          fontWeight: FontWeight.bold,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      ),
+                    ),
+                    FlatButton(
+                      onPressed: () {},
+                      child: const Text("alterar Data"),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 20,
+                left: 40,
+                right: 40,
+                bottom: 10,
+              ),
+              child: TDButton(
+                text: "Salvar",
+                width: double.infinity,
+                callback: () {},
+              ),
+            ),
+            FlatButton(
+              onPressed: () {},
+              child: const Text("Cancelar"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
