376892ff052ea651fd8c5ac848f79860c79f1ad7
stobertonf <stobertonf@gmail.com>
2022-07-17 15:42:16 -0300

    feat(Login-Google): Finishing Login Controller.

--- lib/controllers/login.controller.dart
+++ lib/controllers/login.controller.dart
@@ -6,5 +6,28 @@ class LoginController {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final GoogleSignIn _googleSignIn = GoogleSignIn();
 
-  Future login() async {}
+  Future login() async {
+    final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser.authentication;
+
+    final AuthCredential credential = GoogleAuthProvider.getCredential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    final FirebaseUser firebaseUser =
+        (await _auth.signInWithCredential(credential)).user;
+    var token = await firebaseUser.getIdToken();
+
+    user.token = token.token;
+    user.email = firebaseUser.email;
+    user.name = firebaseUser.displayName;
+    user.picture = firebaseUser.photoUrl;
+  }
+
+  Future logout() async {
+    await FirebaseAuth.instance.signOut();
+    user = IUser();
+  }
 }
