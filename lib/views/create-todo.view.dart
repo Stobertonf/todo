c70ed5679a8a2685b5ee867181bd97f81b571878
stobertonf <stobertonf@gmail.com>
2022-07-17 11:35:13 -0300

    feat(Create-Todo): Creating CreateTodo File.

---
+++ lib/views/create-todo.view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CreateTodoView extends StatelessWidget {
+  const CreateTodoView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
