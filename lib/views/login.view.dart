0faa542a46a56d92e65e3e6fa8d6e6803b23845c
stobertonf <stobertonf@gmail.com>
2022-07-17 15:58:19 -0300

    feat(Login-Google): Adding Busy Method.

--- lib/views/login.view.dart
+++ lib/views/login.view.dart
@@ -54,35 +54,37 @@ class _LoginViewState extends State<LoginView> {
       body: SingleChildScrollView(
         child: Container(
           padding: const EdgeInsets.all(30),
-          child: Card(
-            child: Column(
-              children: <Widget>[
-                const SizedBox(
-                  width: double.infinity,
-                ),
-                Image.asset(
-                  "assets/images/notification.png",
-                  width: 250,
-                ),
-                const Text(
-                  "Olá desconhecido",
-                  style: TextStyle(
-                    fontSize: 20,
+          child: busy == true
+              ? const CircularProgressIndicator()
+              : Card(
+                  child: Column(
+                    children: <Widget>[
+                      const SizedBox(
+                        width: double.infinity,
+                      ),
+                      Image.asset(
+                        "assets/images/notification.png",
+                        width: 250,
+                      ),
+                      const Text(
+                        "Olá desconhecido",
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      TDButton(
+                          text: "Login com Google",
+                          image: "assets/images/google.png",
+                          callback: () {}),
+                      const SizedBox(
+                        height: 40,
+                      ),
+                    ],
                   ),
                 ),
-                const SizedBox(
-                  height: 20,
-                ),
-                TDButton(
-                    text: "Login com Google",
-                    image: "assets/images/google.png",
-                    callback: () {}),
-                const SizedBox(
-                  height: 40,
-                ),
-              ],
-            ),
-          ),
         ),
       ),
     );
