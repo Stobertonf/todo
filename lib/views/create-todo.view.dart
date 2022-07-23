8fdd74c0dcb8cf3dc8fa2ea30ca49775eba0e5de
stobertonf <stobertonf@gmail.com>
2022-07-23 16:30:38 -0300

    feat(Date-Picker): Refactoring Todo-View File.

--- lib/views/create-todo.view.dart
+++ lib/views/create-todo.view.dart
@@ -1,6 +1,12 @@
+import 'home.view.dart';
+import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo/stores/app.store.dart';
+import 'package:todo/models/todo-item.model.dart';
 import 'package:todo/components/button.widget.dart';
 import 'package:todo/widgets/user-card.widget.dart';
+import 'package:todo/controllers/todo.controller.dart';
 
 class CreateTodoView extends StatefulWidget {
   const CreateTodoView({Key? key}) : super(key: key);
@@ -10,8 +16,31 @@ class CreateTodoView extends StatefulWidget {
 }
 
 class _CreateTodoViewState extends State<CreateTodoView> {
+  final _formKey = GlobalKey<FormState>();
+  final _dateFormat = DateFormat('dd/MM/yyyy');
+
+  String task = "";
+  DateTime date = DateTime.now();
+
+  Future<Null> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: date,
+      firstDate: DateTime(2000, 1),
+      lastDate: DateTime(2040),
+    );
+    if (picked != null && picked != date) {
+      setState(() {
+        date = picked;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    final store = Provider.of<AppStore>(context);
+    final controller = new TodoController(store);
+
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
@@ -20,6 +49,7 @@ class _CreateTodoViewState extends State<CreateTodoView> {
             Padding(
               padding: const EdgeInsets.all(40.0),
               child: Form(
+                key: _formKey,
                 child: Column(
                   children: <Widget>[
                     TextFormField(
@@ -27,53 +57,90 @@ class _CreateTodoViewState extends State<CreateTodoView> {
                       decoration: InputDecoration(
                         labelText: "Tarefa",
                         labelStyle: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w400,
                           color: Theme.of(context).primaryColor,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 16,
                         ),
                       ),
                       style: TextStyle(
                         fontSize: 20,
                         color: Theme.of(context).primaryColor,
                       ),
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Título Inválido';
+                        }
+                        return null;
+                      },
+                      onSaved: (val) {
+                        task = val!;
+                      },
                     ),
                     Padding(
                       padding: const EdgeInsets.only(
                         top: 20,
                       ),
                       child: Text(
-                        "17/07/2022",
+                        _dateFormat.format(date),
                         style: TextStyle(
+                          color: Theme.of(context).primaryColor,
                           fontSize: 34,
                           fontWeight: FontWeight.bold,
-                          color: Theme.of(context).primaryColor,
                         ),
                       ),
                     ),
                     FlatButton(
-                      onPressed: () {},
-                      child: const Text("alterar Data"),
-                    ),
+                      child: const Text("Alterar Data"),
+                      onPressed: () {
+                        _selectDate(context);
+                      },
+                    )
                   ],
                 ),
               ),
             ),
             Padding(
               padding: const EdgeInsets.only(
-                top: 20,
-                left: 40,
+                left: 40.0,
                 right: 40,
+                top: 20,
                 bottom: 10,
               ),
               child: TDButton(
+                callback: () {
+                  if (!_formKey.currentState.validate()) {
+                    return;
+                  }
+
+                  _formKey.currentState.save();
+                  var todo = TodoItem(
+                    title: task,
+                    date: date,
+                  );
+
+                  controller.add(todo).then((_) {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const HomeView(),
+                      ),
+                    );
+                  });
+                },
                 text: "Salvar",
                 width: double.infinity,
-                callback: () {},
               ),
             ),
             FlatButton(
-              onPressed: () {},
-              child: const Text("Cancelar"),
+              child: Text("Cancelar"),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const HomeView(),
+                  ),
+                );
+              },
             ),
           ],
         ),
